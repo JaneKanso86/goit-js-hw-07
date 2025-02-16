d53572afea73e79f1ab6e8b1653e6e68d44f2107
Jane Kanso <janekanso86@gmail.com>
2025-02-16 15:58:41 +0300

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,16 @@
+function filterArray(numbers, value) {
+  let newMassive = [];
+  for (let number of numbers) {
+    if (number > value) {
+      newMassive.push(number);
+    }
+  }
+  return newMassive;
+}
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
